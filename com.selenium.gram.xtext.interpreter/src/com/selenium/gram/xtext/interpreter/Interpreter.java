16461344dab0772413d93add86df06315c658b2f
Gwenael Bec <blastgwen@gmail.com>
2015-02-13 08:55:30 +0100

    Create interpreter classes

--- com.selenium.gram.xtext.interpreter/src/com/selenium/gram/xtext/interpreter/Interpreter.java
+++ com.selenium.gram.xtext.interpreter/src/com/selenium/gram/xtext/interpreter/Interpreter.java
@@ -2,4 +2,10 @@ package com.selenium.gram.xtext.interpreter;
 import com.selenium.gram.xtext.slnDsl.*;
 
 public class Interpreter {
+
+	public void execute(Model model) {
+		
+		System.out.println("---------- DEBUT INTERPRETER -------- ");
+		
+	}
 }
