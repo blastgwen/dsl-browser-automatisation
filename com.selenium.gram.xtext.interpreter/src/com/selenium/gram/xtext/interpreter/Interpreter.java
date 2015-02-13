2778b46376771a49b9323657aeeae3c0651668d7
naoskev <vno4@laposte.net>
2015-02-13 09:04:39 +0100

    Correction merge planté

--- com.selenium.gram.xtext.interpreter/src/com/selenium/gram/xtext/interpreter/Interpreter.java
+++ com.selenium.gram.xtext.interpreter/src/com/selenium/gram/xtext/interpreter/Interpreter.java
@@ -2,13 +2,10 @@ package com.selenium.gram.xtext.interpreter;
 import com.selenium.gram.xtext.slnDsl.*;
 
 public class Interpreter {
-<<<<<<< HEAD
-=======
 
 	public void execute(Model model) {
 		
 		System.out.println("---------- DEBUT INTERPRETER -------- ");
 		
 	}
->>>>>>> 16461344dab0772413d93add86df06315c658b2f
 }
